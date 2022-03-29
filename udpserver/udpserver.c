71d2d9ffca86c119f48d593cc3f532c1a637a4de
Luca Bartolomei <bartn8@hotmail.it>
2022-03-29 12:15:36 +0200

    udp example

---
+++ udpserver/udpserver.c
@@ -0,0 +1,74 @@
+// Server side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#define PORT	 11412
+#define BUF_SIZE 1024
+
+
+#if __BIG_ENDIAN__
+# define htonll(x) (x)
+# define ntohll(x) (x)
+#else
+# define htonll(x) (((uint64_t)htonl((x) & 0xFFFFFFFF) << 32) | htonl((x) >> 32))
+# define ntohll(x) (((uint64_t)ntohl((x) & 0xFFFFFFFF) << 32) | ntohl((x) >> 32))
+#endif
+
+typedef struct{
+	int id;
+} my_packet1_t;
+
+// Driver code
+int main() {
+	int sockfd;
+	char buffer[BUF_SIZE];
+	char *hello = "Hello from server";
+	struct sockaddr_in servaddr, cliaddr;
+	
+	// Creating socket file descriptor
+	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
+		perror("socket creation failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	memset(&servaddr, 0, sizeof(servaddr));
+	memset(&cliaddr, 0, sizeof(cliaddr));
+	
+	// Filling server information
+	servaddr.sin_family = AF_INET; // IPv4
+	servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_port = htons(PORT);
+	
+	// Bind the socket with the server address
+	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
+			sizeof(servaddr)) < 0 )
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+	
+	while(1){
+		
+		int len, n;
+		my_packet1_t *frame;
+
+		len = sizeof(cliaddr); //len is value/resuslt
+
+		n = recvfrom(sockfd, (char *)buffer, BUF_SIZE,
+					MSG_WAITALL, ( struct sockaddr *) &cliaddr, len);
+		
+		if(n >= sizeof(app_frame_t)){
+			frame = (my_packet1_t *) buffer;
+			//....
+		}
+	}
+	
+	return 0;
+}
+
